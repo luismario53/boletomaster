59d73218318a72aebceb7a0c751466841746789e
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2025-10-28 16:54:52 -0700

    Creacion de enums
    Se crearon los enums para tratar con distintos modelos

---
+++ constants/enums.js
@@ -0,0 +1,21 @@
+// Enums para el modelo Usuario
+export const TIPO_USUARIO = {
+    CLIENTE: 'Cliente',
+    ORGANIZADOR: 'Organizador',
+    ADMIN: 'Administrador',
+    ARTISTA: 'Artista'
+}
+
+// Enums para el modelo Boleto
+export const TIPO_ASIENTO = {
+    VIP: 'VIP',
+    GENERAL: 'GENERAL',
+    PALCO: 'PALCO'
+}
+
+// Enums para el modelo Orden
+export const ESTADO_ORDEN = {
+    PENDIENTE: 'PENDIENTE',
+    COMPLETADA: 'COMPLETADA',
+    CANCELADA: 'CANCELADA'
+}
